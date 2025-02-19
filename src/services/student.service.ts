6f356629c4613dd914be44db42e43481d0faaa56
rivkamos <77576647+rivkamos@users.noreply.github.com>
2025-02-19 13:00:05 +0200

    change api url

--- src/services/student.service.ts
+++ src/services/student.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Student } from '../models/student.model';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
-  private apiUrl = 'http://localhost:3000/api/students';
+  private apiUrl = `${environment.apiUrl}/students`;
 
   constructor(private http: HttpClient) {}
 
